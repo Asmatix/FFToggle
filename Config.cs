8d473185e113f14627a9aa08f27847957cf39a2a
Frimon22 <152062909+Frimon22@users.noreply.github.com>
2025-01-18 17:23:13 +0100

    Добавьте файлы проекта.

---
+++ Config.cs
@@ -0,0 +1,19 @@
+﻿using Exiled.API.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFToggle
+{
+    public class Config : IConfig
+    {
+        [Description("Whether or not to enable FFToggle.")]
+        public bool IsEnabled { get; set; } = true;
+
+        [Description("Whether or not to enable FFToggle debug logs.")]
+        public bool Debug { get; set; } = false;
+    }
+}
---
+++ FFToggle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.API.Features;
+using Exiled.API.Features.Core.Generic;
+using Exiled.API.Features.Items;
+using Exiled.API.Interfaces;
+using Exiled.Events.EventArgs.Server;
+using Exiled.Events.Handlers;
+using PluginAPI.Events;
+
+
+namespace FFToggle
+{
+    public class FFToggle : Plugin<Config>
+    {
+
+        public override void OnEnabled()
+        {
+            Exiled.Events.Handlers.Server.WaitingForPlayers += WaitingForPlayers;
+            Exiled.Events.Handlers.Server.RoundEnded += RoundEndedEvent;
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Server.WaitingForPlayers -= WaitingForPlayers;
+            Exiled.Events.Handlers.Server.RoundEnded -= RoundEndedEvent;
+            base.OnDisabled();
+        }
+        public void WaitingForPlayers()
+        {
+            Exiled.API.Features.Server.FriendlyFire = false;
+        }
+        public void RoundEndedEvent(RoundEndedEventArgs ev)
+        {
+            Exiled.API.Features.Server.FriendlyFire = true;
+            foreach (Exiled.API.Features.Player player in Exiled.API.Features.Player.List.Where(x => x.IsAlive))
+            {
+                Log.Warn("Видано предмет живим гравцям!");
+                player.AddItem(ItemType.Jailbird);
+            }
+        }
+
+    }
+}
+
+
